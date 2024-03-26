c7dbef44fda0ad432c8d378689b45c4c92bb32b9
Periklis <perik76@gmail.com>
2024-03-26 12:31:17 +0200

    Core done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+const form = document.querySelector('#form')
+const userName = document.querySelector('#name')
+const email = document.querySelector('#email')
+const list = document.querySelector('#list')
+
+
+const checkName = () => {
+    
+}
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const nameLi = document.createElement('li')
+    nameLi.innerText  = userName.value
+    const emailLi = document.createElement('li')
+    emailLi.innerText = email.value
+
+    list.append(nameLi)
+    list.append(emailLi)
+
+    userName.value = ''
+    email.value = ''
+})
 
